c6038d3a6a6b10c5ed78a4004e32c79df7372fa6
Nahuel <nbulian@users.noreply.github.com>
2019-03-08 12:36:07 -0300

    Using Helmet and Morgan, third party middleware
    Helmet helps you secure your Express apps by setting various HTTP headers. https://www.npmjs.com/package/helmet
    
    npm i helmet
    
    Morgan, HTTP request logger. https://www.npmjs.com/package/morgan
    
    npm i morgan

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 // Calss Validator
 const Joi = require('joi'); 
 
+// Helmet (npm)
+const helmet = require('helmet');
+
+// Morgan (npm)
+const morgan = require('morgan');
+
 // My middelware
 const logger = require('./logger');
 
@@ -13,6 +19,8 @@ const app = express();
 app.use(express.json()); // Builtin middelware who parse the request body to json > req.body
 app.use(express.urlencoded( { extended: true } )); // Built-in middleware who convert this key=value&key=value into a json object
 app.use(express.static('public')); // Built-in middleware who published the static content in "public" folder
+app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers.
+app.use(morgan('tiny')); // HTTP request logger
 app.use(logger); // Installing my middelware
 
 const courses = [
