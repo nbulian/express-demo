45f5fe2d863d6c912c855f7473d94d6b6b1c4f38
Nahuel <nbulian@users.noreply.github.com>
2019-03-23 12:08:33 -0300

    Using npm winston package to Logging Errors  A logger for just about everything.  npm i winston  https://www.npmjs.com/package/winston

--- middleware/error.js
+++ middleware/error.js
@@ -1,3 +1,14 @@
+// winston package to Logging Errors
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: 'logfile.log' })
+    ]
+ });
+
 module.exports = function(err, req, res, next){
+    logger.error(err.message, err);
     res.status(500).send('Something failed.');
 }
