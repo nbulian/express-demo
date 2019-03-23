7aa7b2e728a3d7ff6bef48edf1b9392c4209f10b
Nahuel <nbulian@users.noreply.github.com>
2019-03-23 12:30:17 -0300

    Logging using winston-mongodb
    A MongoDB transport for winston: https://www.npmjs.com/package/winston-mongodb
    
    npm i winston-mongodb

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,15 @@
 // winston package to Logging Errors
 const winston = require('winston');
+require('winston-mongodb');
 
 const logger = winston.createLogger({
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: 'logfile.log' })
+      new winston.transports.File({ filename: 'logfile.log' }),
+      new winston.transports.MongoDB({ 
+        db: 'mongodb://localhost/playground',
+        level: 'error'
+      })
     ]
  });
 
