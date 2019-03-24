5769e78c5d650f80aa5b1de667062600aeebe2d5
Nahuel <nbulian@users.noreply.github.com>
2019-03-24 10:45:58 -0300

    Revert "Extracting Routes"
    This reverts commit da9bc3aa03c36c5fea752389a8becff2f4beeee4, reversing
    changes made to bd26d434f119fa4180301924ffa2dea06dc5163e.

--- startup/db.js
+++
@@ -1,7 +0,0 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-
-module.exports = function() {
-    mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB...'));
-}
