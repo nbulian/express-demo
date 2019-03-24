bd26d434f119fa4180301924ffa2dea06dc5163e
Nahuel <nbulian@users.noreply.github.com>
2019-03-24 10:41:14 -0300

    Extracting Routes

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const config = require('config');
 // Debug (npm)
 const debug = require('debug')('app:startup');
 
-// Helmet (npm)
-const helmet = require('helmet');
-
 // Morgan (npm)
 const morgan = require('morgan');
 
@@ -24,18 +21,11 @@ const mongoose = require('mongoose');
 
 // My error middelware
 const logger = require('./middleware/logger'); //General logs
-const error = require('./middleware/error'); //Routes log
-
-// Routes
-const coursesRoutes = require('./routes/courses');
-const authorsRoutes = require('./routes/authors');
-const usersRoutes = require('./routes/users');
-const authRoutes = require('./routes/auth');
-const home = require('./routes/home');
 
 // Build a web server
 const express = require('express');
 const app = express();
+require('./startup/routes')(app);
 
 if (!config.get('jwtPrivateKey')) {
   console.log('FATAL ERROR: jwtPrivateKey is not defined');
@@ -44,17 +34,6 @@ if (!config.get('jwtPrivateKey')) {
 
 app.set('view engine', 'pug');
 app.set('views', './views'); //default
-app.use(express.json()); // Builtin middelware who parse the request body to json > req.body
-app.use(express.urlencoded( { extended: true } )); // Built-in middleware who convert this key=value&key=value into a json object
-app.use(express.static('public')); // Built-in middleware who published the static content in "public" folder
-app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers.
-app.use('/api/courses', coursesRoutes); // Routes for /api/courses
-app.use('/api/authors', authorsRoutes); // Routes for /api/authors
-app.use('/api/users', usersRoutes); // Routes for /api/users
-app.use('/api/auth', authRoutes); // Routes for /api/auth
-app.use('/', home); // Routes for /
-
-app.use(error);
 
 process.on('uncaughtException', (ex) => {
   logger.error(ex.message, ex);
---
+++ startup/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const coursesRoutes = require('../routes/courses');
+const authorsRoutes = require('../routes/authors');
+const usersRoutes = require('../routes/users');
+const authRoutes = require('../routes/auth');
+const home = require('../routes/home');
+const error = require('../middleware/error');
+const helmet = require('helmet');
+
+module.exports = function(app) {
+    app.use(express.json()); // Builtin middelware who parse the request body to json > req.body
+    app.use(express.urlencoded( { extended: true } )); // Built-in middleware who convert this key=value&key=value into a json object
+    app.use(express.static('public')); // Built-in middleware who published the static content in "public" folder
+    app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers.
+    app.use('/api/courses', coursesRoutes); // Routes for /api/courses
+    app.use('/api/authors', authorsRoutes); // Routes for /api/authors
+    app.use('/api/users', usersRoutes); // Routes for /api/users
+    app.use('/api/auth', authRoutes); // Routes for /api/auth
+    app.use('/', home); // Routes for
+    app.use(error);
+}
